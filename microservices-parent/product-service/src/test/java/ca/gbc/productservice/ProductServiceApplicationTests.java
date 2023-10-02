498f6962908e22308a5ec431d1aebd55494286c3
loptopitop <rodrigo.elustondo@gmail.com>
2023-10-02 12:37:28 -0400

    week 5 lecture finished our tests

--- microservices-parent/product-service/src/test/java/ca/gbc/productservice/ProductServiceApplicationTests.java
+++ microservices-parent/product-service/src/test/java/ca/gbc/productservice/ProductServiceApplicationTests.java
@@ -6,6 +6,7 @@ import ca.gbc.productservice.model.Product;
 import ca.gbc.productservice.repository.ProductRepository;
 
 import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.junit.jupiter.api.Assertions;
@@ -18,7 +19,10 @@ import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 
@@ -26,6 +30,8 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -89,7 +95,89 @@ class ProductServiceApplicationTests extends AbstractContainerBaseTest {
         Query query = new Query();
         query.addCriteria(Criteria.where("name").is("Apple iPad 2023"));
         List<Product> product = mongoTemplate.find(query,Product.class);
-        Assertions.assertTrue(product.size() == 1);
+        Assertions.assertTrue(product.size() > 0 );
     }
 
+    @Test
+    void getAllProducts() throws Exception{
+        //Given
+        productRepository.saveAll(getProducts());
+        //When
+        ResultActions response = mockMvc.perform(MockMvcRequestBuilders.get("/api/product")
+                .accept(MediaType.APPLICATION_JSON)
+        );
+        //THEN
+        response.andExpect(MockMvcResultMatchers.status().isOk());
+        response.andDo(MockMvcResultHandlers.print());
+
+        MvcResult result = response.andReturn();
+        String jsonResponse = result.getResponse().getContentAsString();
+        JsonNode jsonNodes = new ObjectMapper().readTree(jsonResponse);
+        int actualSize = jsonNodes.size();
+        int expectedSize = getProducts().size();
+
+        Assertions.assertEquals(expectedSize,actualSize);
+    }
+
+    @Test
+    void updateProducts() throws Exception{
+        //Given
+        Product savedProduct = Product.builder()
+                .id(UUID.randomUUID().toString())
+                .name("Widget")
+                .description("Widget Original Price")
+                .price(BigDecimal.valueOf(100))
+                .build();
+
+        //saved product with original price
+        productRepository.save(savedProduct);
+
+        //prepare updated product and productRequest
+        savedProduct.setPrice(BigDecimal.valueOf(200));
+        String productRequestString = objectMapper.writeValueAsString(savedProduct);
+
+        //When
+        ResultActions response = mockMvc.perform(MockMvcRequestBuilders
+                .put("/api/product/"+ savedProduct.getId().toString())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(productRequestString));
+
+        //Then
+        response.andExpect(MockMvcResultMatchers.status().isNoContent());
+        response.andDo(MockMvcResultHandlers.print());
+
+        Query query = new Query();
+        query.addCriteria(Criteria.where("id").is(savedProduct.getId()));
+        Product storedProduct = mongoTemplate.findOne(query,Product.class);
+
+        assertEquals(savedProduct.getPrice(),storedProduct.getPrice());
+    }
+
+    @Test
+    void deleteProduct() throws Exception{
+        //Given
+        Product savedProduct = Product.builder()
+                .id(UUID.randomUUID().toString())
+                .name("Java Microservices Programming")
+                .description("Course Textbook = Java Microservices Programming")
+                .price(BigDecimal.valueOf(200))
+                .build();
+        productRepository.save(savedProduct);
+
+        //When
+        ResultActions response = mockMvc.perform(MockMvcRequestBuilders
+                        .delete("/api/product/"+ savedProduct.getId().toString())
+                        .contentType(MediaType.APPLICATION_JSON));
+        //Then
+        response.andExpect(MockMvcResultMatchers.status().isNoContent());
+        response.andDo(MockMvcResultHandlers.print());
+
+        Query query = new Query();
+        query.addCriteria(Criteria.where("id").is(savedProduct.getId()));
+        Long productCount = mongoTemplate.count(query,Product.class);
+
+        assertEquals(0,productCount);
+    }
 }
+
+
